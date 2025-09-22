84b089d77e8a0f6450506e0023c41abd114581aa
zafid-affan-2 <email.zafidaffan@gmail.com>
2025-09-22 11:37:00 +0700

    Update db.js

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ const mysql = require('mysql2');
 
 const pool = mysql.createPool({
   host: process.env.DB_HOST,     
-  port: 31481,
+  port: process.env.DB_PORT,
   user: process.env.DB_USER,     
   password: process.env.DB_PASS, 
   database: process.env.DB_NAME,
